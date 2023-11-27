d2ebcd11a297c32cd96ee607bfa2937d3f321010
Nikolas Costa <135657115+nikolashnc@users.noreply.github.com>
2023-11-27 11:43:37 -0500

    Update SALES_ANALYSIS.sql

--- SALES_ANALYSIS.sql
+++ SALES_ANALYSIS.sql
@@ -101,7 +101,7 @@ SELECT CUSTOMERNAME,RFM_RECENCY,RFM_FREQUENCY,RFM_MONETARY,
 
 case 
 		when rfm_cell_string in (111, 112 , 121, 122, 123, 132, 211, 212, 114, 141) then 'lost_customers'  --lost customers
-		when rfm_cell_string in (133, 134, 143, 244, 334, 343, 344, 144) then 'slipping away, cannot lose' -- (Big spenders who haven’t purchased lately) slipping away
+		when rfm_cell_string in (133, 134, 143, 244, 334, 343, 344, 144) then 'slipping away, cannot lose' -- (Big spenders who havenâ€™t purchased lately) slipping away
 		when rfm_cell_string in (311, 411, 331) then 'new customers'
 		when rfm_cell_string in (222, 223, 233, 322) then 'potential churners'
 		when rfm_cell_string in (323, 333,321, 422, 332, 432) then 'active' --(Customers who buy often & recently, but at low price points)
